b234ab10da37c79486895d3c0751a62f72432b71
Juan <juansw03@gmail.com>
2024-02-07 15:18:30 -0500

    feat: arrow to indicate that there is more project information

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,11 +6,12 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { badges } from '@/shared/badges';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 
-export default function ProjectCard({project}) {
+export default function ProjectCard({ project }) {
     const { t } = useTranslation()
 
     const openInNewTab = (url) => {
@@ -18,46 +19,49 @@ export default function ProjectCard({project}) {
         if (newWindow) newWindow.opener = null
     }
 
-  return (
-    <Card className='group relative aspect-video max-w-[550px]'>
-        <CardActionArea onClick={() => openInNewTab(project.live)}>
-            <CardMedia
-                component="img"
-                height="140"
-                image={project.image}
-                alt="green iguana"
-                className='z-0 relative'
-            />
-        </CardActionArea>
-        <CardContent className='bg-bgWhite dark:bg-[#181818] dark:text-pWhite text-pBlack !p-3 h-full absolute z-10 bottom-0 max-h-14 w-full  group-hover:max-h-44 transition-all duration-500 overflow-hidden '>
-            <Typography 
-                gutterBottom
-                variant="h3" 
-                component="div" 
-                className='!text-2xl custom-font !font-semibold'
-            >
-                {project.title}
-            </Typography>
-            <article className='flex flex-col justify-between h-3/4'>
-                <Typography variant="body2" className='custom-font'>
-                    {t(project.description)}
-                </Typography>
-                <div className='flex items-center justify-between justify-self-end '>
-                    <div className='flex gap-1 lg:gap-2 items-center'>
-                        <a href={project.repo} target='_blank'>
-                            <GitHubIcon className='cursor-pointer' />
-                        </a>
-                        <a href={project.live} target='_blank'>
-                            <OpenInNewIcon className='cursor-pointer' />
-                        </a>
-                    </div>
-                    <div className='flex gap-2 items-center'>
-                        {project.technologies.map( tech => <Image width={tech==='javascript'?30:24} height={tech==='javascript'?30:24} src={badges[tech]} key={tech} alt={`${tech} badge`}/>)}
-                    </div>
+    return (
+        <Card className='group relative aspect-video max-w-[550px]'>
+            <CardActionArea onClick={() => openInNewTab(project.live)}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={project.image}
+                    alt="green iguana"
+                    className='z-0 relative'
+                />
+            </CardActionArea>
+            <CardContent className='bg-bgWhite dark:bg-[#181818] dark:text-pWhite text-pBlack !p-3 h-full absolute z-10 bottom-0 max-h-14 w-full  group-hover:max-h-44 transition-all duration-500 overflow-hidden '>
+                <div className='flex justify-between items-start'>
+                    <Typography
+                        gutterBottom
+                        variant="h3"
+                        component="div"
+                        className='!text-2xl custom-font !font-semibold clamp'
+                    >
+                        {project.title}
+                    </Typography>
+                    <KeyboardArrowDownIcon className='group-hover:rotate-180 transition-all duration-75' />
                 </div>
-            </article>
-        </CardContent>
-      
-    </Card>
-  );
+                <article className='flex flex-col justify-between h-3/4'>
+                    <Typography variant="body2" className='custom-font'>
+                        {t(project.description)}
+                    </Typography>
+                    <div className='flex items-center justify-between justify-self-end '>
+                        <div className='flex gap-1 lg:gap-2 items-center'>
+                            <a href={project.repo} target='_blank'>
+                                <GitHubIcon className='cursor-pointer' />
+                            </a>
+                            <a href={project.live} target='_blank'>
+                                <OpenInNewIcon className='cursor-pointer' />
+                            </a>
+                        </div>
+                        <div className='flex gap-2 items-center'>
+                            {project.technologies.map(tech => <Image width={tech === 'javascript' ? 30 : 24} height={tech === 'javascript' ? 30 : 24} src={badges[tech]} key={tech} alt={`${tech} badge`} />)}
+                        </div>
+                    </div>
+                </article>
+            </CardContent>
+
+        </Card>
+    );
 }
