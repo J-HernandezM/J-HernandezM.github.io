880942df1b7336b184f78a038099765fd90f2542
Juan <juansw03@gmail.com>
2024-07-10 01:41:51 -0500

    feat: new change language button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import SwitchDarkMode from './Switch';
 
 export default function Header() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  const changeLang = () => {
+    if (language === 'es') {
+      i18n.changeLanguage('en');
+      setLanguage('en');
+    } else {
+      i18n.changeLanguage('es');
+      setLanguage('es');
+    }
+  };
 
   return (
     <nav className="flex py-5 lg:py-10 justify-between items-center md:text-base lg:text-lg bg-transparent w-full fixed top-0 z-50 backdrop-filter backdrop-blur text-opacity-100 dark:border-pWhite border-pBlack border-b">
@@ -21,7 +33,12 @@ export default function Header() {
           <a href="#projects">04. {t('projects')}</a>
         </li>
       </ul>
-      <SwitchDarkMode />
+      <div className="flex items-center gap-4 ">
+        <button onClick={changeLang} className="cursor-pointer text-xs md:text-sm w-7 h-7 hover:scale-110 transition-transform">
+          {language === 'es' ? 'EN' : 'ES'}
+        </button>
+        <SwitchDarkMode />
+      </div>
     </nav>
   );
 }
